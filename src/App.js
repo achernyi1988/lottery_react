ed5b9b3d98e3d98d1b7d74f994098c1324a6bbdb
achernyi <ote1188@gmail.com>
2018-12-04 00:05:25 +0200

    finished the building interactive front-ends

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from "./web3";
 import lottery from "./lottery"
+
 
 export default class App extends Component {
 
@@ -10,32 +10,63 @@ export default class App extends Component {
 
     constructor(props){
         super(props);
-
-        this.state =  {manager : "",
+        this.state =  {
+            manager : "",
             players: [] ,
             balance: "",
-            value:""};
+            value:"",
+            message: ""};
 
     }
+    onEnter = async (event) =>{
+        event.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({message: "waiting on transaction success ..."});
+
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
+        });
+        await this.updateStates();
+        this.setState({message: "You have been entered!"});
+
+    };
+
+    onPickWinner = async () => {
+        const accounts = await web3.eth.getAccounts();
+        this.setState({message: "waiting on transaction success ..."});
+
+        await lottery.methods.pickWinner().send({
+            from: accounts[0]});
+
+        await this.updateStates();
+        this.setState({message: "The winner has been picked!"});
+
+    };
 
      async componentDidMount(){
+         await this.updateStates();
+    }
 
-         const manager = await lottery.methods.manager().call();
-         const players = await lottery.methods.getPlayers().call();
-         const balance = await web3.eth.getBalance(lottery.options.address);
-
-         this.setState({manager, players, balance});
+    updateStates = async() => {
+        const manager = await lottery.methods.manager().call();
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+        this.setState({manager, players, balance});
     }
   render() {
       return (
     <div>
         <h2> Lottery Contract </h2>
         <p>  This contract is managed by {this.state.manager}
-            There are currently {this.state.players.length} people entered <br></br>
-            competing to win {web3.utils.fromWei(this.state.balance, "ether")} ehter!
+        <br></br> There are currently {this.state.players.length} people entered
+            competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!
+
         </p>
         <hr/>
-        <form>
+        <form onSubmit={this.onEnter}>
             <h4> Want to try your like?</h4>
             <div>
                 <label>Amount of ether to enter</label>
@@ -46,6 +77,13 @@ export default class App extends Component {
             </div>
             <button>Enter</button>
         </form>
+        <hr    />
+        <h4>Ready to pick a winner</h4>
+        <button onClick = {this.onPickWinner} > Pick a winner</button>
+        <hr/>
+
+        <h1> {this.state.message}</h1>
+
     </div>
     );
   }
