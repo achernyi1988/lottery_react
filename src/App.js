42471a450907f2de637a55d20b85555b16f9fc4e
achernyi <achernyi@luxoft.com>
2018-12-03 19:24:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from "./web3";
+import lottery from "./lottery"
 
-class App extends Component {
+export default class App extends Component {
+
+    //state = { manager:""};ES6
+
+    constructor(props){
+        super(props);
+
+        this.state =  {manager : "",
+            players: [] ,
+            balance: "",
+            value:""};
+
+    }
+
+     async componentDidMount(){
+
+         const manager = await lottery.methods.manager().call();
+         const players = await lottery.methods.getPlayers().call();
+         const balance = await web3.eth.getBalance(lottery.options.address);
+
+         this.setState({manager, players, balance});
+    }
   render() {
       return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div>
+        <h2> Lottery Contract </h2>
+        <p>  This contract is managed by {this.state.manager}
+            There are currently {this.state.players.length} people entered <br></br>
+            competing to win {web3.utils.fromWei(this.state.balance, "ether")} ehter!
+        </p>
+        <hr/>
+        <form>
+            <h4> Want to try your like?</h4>
+            <div>
+                <label>Amount of ether to enter</label>
+                <input
+                    value = {this.state.value}
+                    onChange = {event => this.setState({value: event.target.value})}
+                />
+            </div>
+            <button>Enter</button>
+        </form>
+    </div>
     );
   }
 }
 
-export default App;
--- src/lottery.js
+++ src/lottery.js
@@ -1,1 +1,8 @@
-import web3 from "web3"
+import web3 from "./web3";
+
+const address = "0x7B76F4879d4D437D9d29521b1ee5C1b9EbeFC74b";
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+
+export default new web3.eth.Contract(abi,address);
